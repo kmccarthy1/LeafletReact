8e57fb3ef710e7afbcd97d82fc4f85e196d97534
Kevin McCarthy <km.mccarthy11@gmail.com>
2022-08-15 20:34:57 -0400

    initial setup

---
+++ leafletreact/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple Leaflet Project</h1>
+    </div>
+  );
+}
+
+export default App;
